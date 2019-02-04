5f6f4f6318dd68aec2f9722db31cbc4283bcb8c7
John Kawahara <kawaharajohn@gmail.com>
2019-02-03 19:19:40 -0800

    Update routing indexing; need to debug authentication routing

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,19 @@
 const path = require("path");
 const router = require("express").Router();
 const apiRoutes = require("./apiRoutes");
+const isAuthenticated = require("../config/middleware/isAuthenticated");
 
 // API Routes
-router
-  .use("/api", apiRoutes)
+router.use("/api", apiRoutes)
+
+// router
+//   .get("/user", isAuthenticated, function(req, res) {
+//     // res.sendFile(path.join(__dirname, "../public/user-page.html"));
+//     res.json(res)
+//   });
 
 router.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"))
 })
-
-// If no API routes are hit, send the React app
-// router.get(function(req, res) {
-//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-// });
 
 module.exports = router;
