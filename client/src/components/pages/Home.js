c8d95050a05353a1f4c7aaf6a3ae9f28e33e6f9a
jarrenpatao <jarrenpatao@gmail.com>
2019-02-04 12:02:29 -0800

    changing some styling to login/nav

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,16 +1,18 @@
 import React from "react";
+import Lesson from "./Lesson";
 
 
 var sectionStyle = {
+  padding: "25px",
   width: "100%",
   height: "100vh"
 };
 function Home() {
   return (
-    <div style={sectionStyle} >
-    <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+    <div style={sectionStyle}>
     {/* if material bootstrap is installed, you should see the below button as green */}
     <button className="btn-default">Welcome to Fullstack</button>
+    {Lesson}
     </div>
   );
 }
