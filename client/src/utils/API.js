8bb8b50e210b92c06baeb9cf10a824a4a901e721
John Kawahara <kawaharajohn@gmail.com>
2019-02-04 18:08:04 -0800

    Update loginUser route with user data object

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,8 +4,8 @@ import axios from "axios";
 // Export axios GET, POST
 export default {
   // Login user with the given id
-  loginUser: function(id) {
-    return axios.get("/api/login/" + id);
+  loginUser: function(userData) {
+    return axios.get("/api/login", userData);
   },
   // Gets user with the given id
   getUser: function(id) {
