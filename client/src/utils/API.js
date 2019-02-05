39e14ad95aa3d71e306b6095f88b7de0d7600b47
John Kawahara <kawaharajohn@gmail.com>
2019-02-04 17:14:38 -0800

    Add loginUser and getUser axios gets

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,6 +3,10 @@ import axios from "axios";
 
 // Export axios GET, POST
 export default {
+  // Login user with the given id
+  loginUser: function(id) {
+    return axios.get("/api/login/" + id);
+  },
   // Gets user with the given id
   getUser: function(id) {
     return axios.get("/api/users/" + id);
