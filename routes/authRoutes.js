b187cb4ca26dc928622380e72702988423f31cb4
Parashara <parashara@gmail.com>
2019-02-05 08:08:59 -0800

    working login and redirect. must add a users class if sign up new user

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,7 @@
 // *** Include Modules: passport
 const passport = require("../config/passport");
 const isAuthenticated = require("../config/middleware/isAuthenticated");
+const db = require("../models");
 
 module.exports = function(app) {
   app.post("/api/login", passport.authenticate("local"), function(req, res) {
@@ -9,14 +10,37 @@ module.exports = function(app) {
 
   app.get("/api/login", isAuthenticated, function (req, res) {
     console.log("authenticated");
+    console.log(req.user)
     if (req.user) {
-      return res.redirect("/profile/" + req.user.id)
+      return res.json("/profile/" + req.user.id)
     }
-    return res.redirect("/signup");
+    return res.json("/signup");
   })
 
   app.get("/logout", function(req, res) {
     req.logout();
     return res.redirect("/");
   });
+
+  app.get("/profile/:id", isAuthenticated, function(req, res) {
+    console.log(req.user)
+    db.User
+    .findOne({ where: { id: req.params.id } })
+    .then(dbModel => res.json(dbModel))
+    .catch(err => res.status(422).json(err));
+})
+  app.get("/profile/class/:id", isAuthenticated, function(req, res) {
+    db.Class
+    .findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [
+        db.Lesson
+      ]
+    })
+    .then(dbModel => res.json(dbModel))
+    .catch(err => res.status(422).json(err));
+  })
+
 };
