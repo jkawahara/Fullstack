f0f9571c123d02f038d95c9ed97dda1141da1693
John Kawahara <kawaharajohn@gmail.com>
2019-02-04 17:12:58 -0800

    Update Login component with handlers and render logic, using FormInput component

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,45 +1,90 @@
-import React from "react";
+// *** Include Modules: npm (react, mdbreact), /utils
+import React, { Component } from "react";
 import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
-import "./style.css"
-
-var sectionStyle = {
-  width: "100%",
-  height: "100vh"
-};
+import API from "../../utils/API";
+import { FormInput, FormBtn } from "../FormInput";
+import "./style.css";
 
 class Login extends React.Component {
-    state = {
-      login: ''
+  state = {
+    user: {},
+    email: "",
+    password: "",
+  };
+
+  // Loads user profile and sets to this.state.user
+  loadUser = () => {
+    API.getUser()
+      .then(res => {
+        this.setState({ user: res.data, email: "", password: "" })
+      })
+      .catch(err => console.log(err));
     };
-
-render() { 
-  return (
-    <div style={sectionStyle} >
-    {/* if material bootstrap is installed, you should see the below button as green */}
   
-    <MDBContainer>
-      <MDBRow>
-        <MDBCol md="4">{/* Grid Spacer */}</MDBCol>
-        <MDBCol md="4">
-          <div className="form-group">
-            <input size="30" type="email" className="form-control" placeholder="Your e-mail" />
-          </div>
-          <div className="form-group">
-            <input size="30" type="password" className="form-control" placeholder="Your password" />
-          </div>
-          <button className="btn-default">Login</button> 
-          <hr />
-          <p>or login with Google</p>
-          <div className="nav-item g-signin2 center" data-onsuccess="onSignIn"></div>
+  // Handles updating component state when the user types into the input field
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
 
-        </MDBCol>
-        <MDBCol md="4">{/* Grid Spacer */}</MDBCol>
-      </MDBRow>
-    </MDBContainer>
+  // Handles login
+  handleLoginSubmit = event => {
+    event.preventDefault();
+    if (this.state.email && this.state.password) {
+      API.loginUser({
+        email: this.state.email,
+        password: this.state.password
+      })
+        .then(() => {
+          this.setState({
+            email: "",
+            password: ""
+          });
+          this.loadUser();
+        })
+        .catch(err => console.log(err));
+    }
+  };
 
-    </div>
-     
-  );
-}
+  render() { 
+    return (
+      <div>
+        <MDBContainer>
+          <MDBRow>
+            <MDBCol md="4">{/* Grid Spacer */}</MDBCol>
+            <MDBCol md="4">
+              <form>
+                <FormInput
+                  value={this.state.email}
+                  onChange={this.handleInputChange}
+                  name="email"
+                  type="email"
+                  placeholder="Your e-mail"
+                />
+                <FormInput
+                  value={this.state.password}
+                  onChange={this.handleInputChange}
+                  name="password"
+                  type="password"
+                  placeholder="Your password"
+                />
+                <FormBtn
+                  onClick={this.handleLoginSubmit}
+                >
+                  Login
+                </FormBtn>
+              </form>
+              {/* <hr />
+              <p>or login with Google</p>
+              <div className="nav-item g-signin2 center" data-onsuccess="onSignIn"></div> */}
+            </MDBCol>
+            <MDBCol md="4">{/* Grid Spacer */}</MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </div>
+    );
+  }
 }
 export default Login;
