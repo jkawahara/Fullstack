cf24371409ee871f4e585267d7064af3c7b724df
John Kawahara <kawaharajohn@gmail.com>
2019-02-04 17:13:55 -0800

    Remove unused middleware require

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,9 @@
 const path = require("path");
 const router = require("express").Router();
 const apiRoutes = require("./apiRoutes");
-const isAuthenticated = require("../config/middleware/isAuthenticated");
 
 // API Routes
 router.use("/api", apiRoutes)
-
-// router
-//   .get("/user", isAuthenticated, function(req, res) {
-//     // res.sendFile(path.join(__dirname, "../public/user-page.html"));
-//     res.json(res)
-//   });
 
 router.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"))
