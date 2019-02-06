e9ab2531fda7a0dfc5095da949a45c462295e23a
Parashara <parashara@gmail.com>
2019-02-05 16:21:04 -0800

    initial setup for addlesson

---
+++ client/src/components/pages/AddLesson.js
@@ -0,0 +1,91 @@
+// *** Include Modules: npm (react, mdbreact), /utils
+import React from "react";
+import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
+import API from "../../utils/API.js";
+import { Redirect } from "react-router-dom";
+
+class SignUp extends React.Component {
+  state = {
+    name: "",
+    email: "",
+    password: "",
+    class: "",
+    redirect: false
+  };
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    API.saveUser({
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+      class: this.state.class
+    }).then((res) => {
+      this.setState({
+        name: "",
+        email: "",
+        password: "",
+        class: "",
+      });
+      this.setRedirect();
+      this.renderRedirect();
+    })
+  };
+
+  setRedirect = () => {
+    this.setState({
+      redirect: true
+    })
+  }
+
+  renderRedirect = () => {
+    if (this.state.redirect) {
+      return <Redirect to="/login" />
+    }
+  }
+
+  render() {
+    return (
+      <div >
+        <MDBContainer>
+          <MDBRow>
+            <MDBCol md="4"></MDBCol>
+            <MDBCol md="4">
+              <form className="form"> {/* //for comp */}
+                <div className="form-group">
+                  <input size="30" name="name" className="form-control" placeholder="Lesson Name" value={this.state.name} onChange={this.handleInputChange} />
+                </div>
+                <div className="form-group">
+                  <input size="30" name="email" className="form-control" placeholder="Lesson URL" value={this.state.email} onChange={this.handleInputChange} />
+                </div>
+                <div className="form-group">
+                  <input size="30" name="password" className="form-control" placeholder="Your password" value={this.state.password} onChange={this.handleInputChange} />
+                </div>
+                <div className="form-group">
+                <select name="class" className="browser-default custom-select" onChange={this.handleInputChange} >
+                  <option>-- Class for Lesson --</option>
+                  <option value="1">UCB</option>
+                  <option value="2">Bootcamp2</option>
+                  <option value="3">Bootcamp3</option>
+                </select>
+                </div>
+                <button onClick={this.handleFormSubmit} type="submit" className="btn btn-lg btn-danger float-right">
+                  Sign Up
+                </button>
+                { this.renderRedirect() }
+              </form>
+            </MDBCol>
+            <MDBCol md="4"></MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </div>
+    );
+  }
+}
+export default SignUp;
